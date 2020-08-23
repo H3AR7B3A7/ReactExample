dbc50bef8e4d70805b2738dfe0a731a49713c934
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2020-08-23 17:34:15 +0200

    Passing props and header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/layout/Header'
 import Todos from './components/Todos';
 
 class App extends Component {
@@ -23,10 +24,31 @@ class App extends Component {
             }
         ]
     }
+
+    // Toggle Completed
+    toggleComplete = (id) => {
+        this.setState({
+            todos: this.state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo;
+            })
+        });
+    }
+
+    // Delete Todo
+    delTodo = (id) => {
+        this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+    }
+
     render() {
         return (
             <div className="App">
-                <Todos todos={this.state.todos} />
+                <Header/>
+                <Todos todos={this.state.todos}
+                    markComplete={this.toggleComplete}
+                    delTodo={this.delTodo} />
             </div>
         );
     }
